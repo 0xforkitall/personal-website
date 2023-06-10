e5c6ed238e09cef3be884dc5c7edd7ce98a1cc43
0xforkitall <0xforkitall@gmail.com>
2023-06-10 17:12:16 +0200

    Update public data, setup 0xforkitall configs

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ["@0xforkitall/eslint-config"],
+    parserOptions: {
+      project: 'tsconfig.json',
+      tsconfigRootDir: __dirname,
+  },
+};
---
+++ src/shared/components/button/button.tsx
@@ -0,0 +1,13 @@
+import classNames from 'classnames';
+import React, { type HTMLAttributes } from 'react';
+
+export interface IButtonProps extends HTMLAttributes<HTMLButtonElement> {
+    /**
+     * Custom class for the component.
+     */
+    className?: string;
+}
+
+export const Button: React.FC<IButtonProps> = ({ className }) => {
+    return <button className={classNames('button', className)} />;
+};
