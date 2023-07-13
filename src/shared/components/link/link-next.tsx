b6139c8006146416da2caf523108dee90b04f635
0xforkitall <0xforkitall@gmail.com>
2023-07-13 19:18:38 +0200

    Update link component to handle disabled state and hover-underline variant

--- src/shared/components/link/link-next.tsx
+++ src/shared/components/link/link-next.tsx
@@ -5,6 +5,16 @@ import type { ILinkProps } from './link.api';
 
 export interface ILinkNextProps extends LinkProps, Pick<ILinkProps, 'color' | 'size'> {
     /**
+     * Variant of the link.
+     * @default default
+     */
+    variant?: 'default' | 'hover-underline';
+    /**
+     * Disables the button when set to true.
+     * @default false
+     */
+    disabled?: boolean;
+    /**
      * Children of the component.
      */
     children?: ReactNode;
@@ -15,10 +25,29 @@ export interface ILinkNextProps extends LinkProps, Pick<ILinkProps, 'color' | 's
 }
 
 export const LinkNext: React.FC<ILinkNextProps> = (props) => {
-    const { color = 'inherit', size = 'm', className, children, ...otherProps } = props;
+    const {
+        variant = 'default',
+        color = 'inherit',
+        size = 'm',
+        className,
+        disabled = false,
+        children,
+        ...otherProps
+    } = props;
 
     return (
-        <Link className={classNames('link', `link--size-${size}`, `link--color-${color}`, className)} {...otherProps}>
+        <Link
+            aria-disabled={disabled}
+            className={classNames(
+                'link',
+                `link--variant-${variant}`,
+                `link--size-${size}`,
+                `link--color-${color}`,
+                { 'link--disabled': disabled },
+                className,
+            )}
+            {...otherProps}
+        >
             {children}
         </Link>
     );
